0a330ee0346c931c2a87d386f60cfc844831e7fc
Yevhenii Pozniak <jekamanu@gmail.com>
2025-12-29 20:19:31 +0200

    ch02 update

---
+++ ch02/00-helloworld.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  //   res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.end("Hello World!");
+  res.end("<h1>Hello World!</h1>");
+});
+
+server.listen(port, () =>
+  console.log(
+    `server started on port ${port}; ` + "press Ctrl-C to terminate...."
+  )
+);
